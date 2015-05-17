e88201e97fadb0dfbc30e8ee00a92cf61518a535
Dax <d.dax@email.com>
2015-05-17 22:46:19 +0200

    Update moon presenter add tests

---
+++ spec/presenters/moon_presenter_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+class MoonPresenterSpec < TarotSpec
+  let(:options) do
+    {
+      :phase => phase,
+      :illumination => illumination,
+      :is_waxing => is_waxing,
+      :is_waning => is_waning
+    }
+  end
+  let(:moon) { OpenStruct.new(options) }
+  let(:phase) { :new }
+  let(:illumination) { 0 }
+  let(:is_waxing) { false }
+  let(:is_waning) { false }
+  
+  let(:presenter) { MoonPresenter.new(moon) }
+
+  it 'gives access to phase' do 
+    assert_equal phase, presenter.phase 
+  end
+
+  it 'gives access to percent_illuminated' do 
+    assert_equal illumination * 100, presenter.percent_illuminated
+  end
+
+  it 'gives access to is_waxing' do 
+    assert_equal is_waxing, presenter.waxing?
+  end
+
+  it 'gives access to is_waning' do 
+    assert_equal is_waning, presenter.waning?
+  end
+
+  describe 'image_path' do 
+    it 'returns the correct image path' do
+      assert_equal '/images/luna/0.png', presenter.image_path
+    end
+
+    describe '23% waxing'do
+      let(:phase) { :crescent }
+      let(:illumination) { 0.23 }
+      let(:is_waxing) { true }
+    
+      it 'returns the correct image path' do
+        assert_equal '/images/luna/3.png', presenter.image_path
+      end
+    end
+
+    describe '23% waning' do
+      let(:phase) { :balsamic }
+      let(:illumination) { 0.23 }
+      let(:is_waning) { true }
+    
+      it 'returns the correct image path' do 
+        assert_equal '/images/luna/24.png', presenter.image_path
+      end
+    end
+  end
+end
